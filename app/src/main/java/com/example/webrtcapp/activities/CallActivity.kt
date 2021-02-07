d3bb12040279743d98e9e72f7c13d0eff93f7611
Gh0sT <iammohit810@gmail.com>
2021-02-07 20:51:41 +0530

    refactoring

--- app/src/main/java/com/example/webrtcapp/activities/CallActivity.kt
+++ app/src/main/java/com/example/webrtcapp/activities/CallActivity.kt
@@ -2,11 +2,61 @@ package com.example.webrtcapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.example.webrtcapp.R
+import kotlinx.android.synthetic.main.activity_call.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class CallActivity : AppCompatActivity() {
+
+    var uid: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_call)
+
+        uid = intent.getStringExtra("uid")
+        setupWebView()
+
     }
+
+    private fun setupWebView() {
+
+        webView.webChromeClient = object: WebChromeClient() {
+            override fun onPermissionRequest(request: PermissionRequest?) {
+                request?.grant(request.resources)
+            }
+        }
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.mediaPlaybackRequiresUserGesture = false
+        //webView.addJavascriptInterface(JavascriptInterface(this), "Android")
+
+        loadVideoCall()
+    }
+
+    private fun loadVideoCall() {
+        val filePath = "file:android_asset/index.html"
+        webView.loadUrl(filePath)
+
+        webView.webViewClient = object: WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                initializePeer()
+            }
+        }
+    }
+
+    private fun initializePeer() {
+
+        callJavascriptFunction("javascript:init()")
+
+    }
+
+    private fun callJavascriptFunction(functionString: String) {
+        webView.post { webView.evaluateJavascript(functionString, null) }
+    }
+
 }
--- app/src/main/java/com/example/webrtcapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/webrtcapp/activities/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.webrtcapp.activities
 
 import android.Manifest
 import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.text.TextUtils
 import android.webkit.PermissionRequest
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.example.webrtcapp.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -26,43 +29,18 @@ class MainActivity : Activity() {
             askPermissions()
         }
 
-        setupWebView()
-    }
-
-    private fun setupWebView() {
-
-        webView.webChromeClient = object: WebChromeClient() {
-            override fun onPermissionRequest(request: PermissionRequest?) {
-                request?.grant(request.resources)
+        button.setOnClickListener {
+            if (isPermissionGranted() && !TextUtils.isEmpty(editTextUnique.text)) {
+                val intent = Intent(this, CallActivity::class.java)
+                intent.putExtra("uid", editTextUnique.text)
+                startActivity(intent)
+            } else if (!isPermissionGranted()) {
+                askPermissions()
+            } else if (TextUtils.isEmpty(editTextUnique.text)) {
+                Toast.makeText(this, "Please enter a unique id in the text box.",Toast.LENGTH_SHORT).show()
             }
         }
 
-        webView.settings.javaScriptEnabled = true
-        webView.settings.mediaPlaybackRequiresUserGesture = false
-        //webView.addJavascriptInterface(JavascriptInterface(this), "Android")
-
-        loadVideoCall()
-    }
-
-    private fun loadVideoCall() {
-        val filePath = "file:android_asset/index.html"
-        webView.loadUrl(filePath)
-
-        webView.webViewClient = object: WebViewClient() {
-            override fun onPageFinished(view: WebView?, url: String?) {
-                initializePeer()
-            }
-        }
-    }
-
-    private fun initializePeer() {
-
-        callJavascriptFunction("javascript:init()")
-
-    }
-
-    private fun callJavascriptFunction(functionString: String) {
-        webView.post { webView.evaluateJavascript(functionString, null) }
     }
 
     private fun askPermissions() {
